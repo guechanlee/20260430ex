cb33a12f59c54191eec25b751c407967d82b660d
guechanlee <guechanlee@google.com>
2026-04-30 17:22:35 +0900

    last

--- ex001/useVariavle.py
+++ ex001/useVariavle.py
@@ -1,27 +1,14 @@
-str = '192.168.56.101'
-
-print(str)
-print(str)
-print(str)
-print(str)
-print(str)
-
-# 데이터 복사
-var1 = 123
-var2 = var1
-
 
 num1 = 111
 num2 = 222
 
-num1 = num2
-num2 = num1
 
 
+name = '이규찬'
+numbers = 10000
+day = 23
 
-print(num1)
-print(num2)
-
-num1, num2 = num2, num1
-print(num1)
-print(num2)
+print(name + '고객님의 결제금액과 납부일자를 보내드릴려고 합니다')
+print(name + '고객님의 결제금액은 ' + str(numbers) +'원입니다')
+print(name + '고객님의 납부일자는 ' + str(day) +'일입니다')
+print(name + '님 항상 행복하시고 건강하세요')
