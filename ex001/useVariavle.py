013b76df5f47a1792d38e141fc5865eed4f0403e
guechanlee <guechanlee@gmail.com>
2026-05-01 13:02:37 +0900

    First Mac

--- ex001/useVariavle.py
+++ ex001/useVariavle.py
@@ -1,14 +1,9 @@
+a = 101
+b = 202
 
-num1 = 111
-num2 = 222
+ol = a
+a = b
+b = ol
 
-
-
-name = '이규찬'
-numbers = 10000
-day = 23
-
-print(name + '고객님의 결제금액과 납부일자를 보내드릴려고 합니다')
-print(name + '고객님의 결제금액은 ' + str(numbers) +'원입니다')
-print(name + '고객님의 납부일자는 ' + str(day) +'일입니다')
-print(name + '님 항상 행복하시고 건강하세요')
+print(a)
+print(b)
