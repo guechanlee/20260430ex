134587f8ebfc15dd9ea195bd5a964f0b659e6ad2
guechanlee <guechanlee@gmail.com>
2026-05-05 16:53:02 +0900

    2026505

--- ex001/useVariavle.py
+++ ex001/useVariavle.py
@@ -7,3 +7,27 @@ b = ol
 
 print(a)
 print(b)
+
+
+
+a = '규찬이는'
+b = '서린이는'
+c = '재연이는'
+
+
+for n in range(1,31,10):
+    print(f'{n}번은 {n}번부터 {n+9}번까지 모아줘')
+
+
+
+
+score = int(input('나이를 입력하시오'))
+
+if score <= 10:
+    print('유아기')
+elif score <= 20:
+    print('청소년기')
+elif score <= 30:
+    print('성인기')
+
+    
